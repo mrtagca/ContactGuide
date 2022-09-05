0a1b76b84d1278c844f313774a0ae73d8aef87ef
mertagca <mrtagcaa@gmail.com>
2022-09-04 21:14:20 -0700

    request & response base types added

---
+++ BaseTypes/Request/RequestBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseTypes.Request
+{
+    public class RequestBase : IDisposable
+    {
+        public RequestBase()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            GC.Collect();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ BaseTypes/Response/ResponseBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseTypes.Response
+{
+    public class ResponseBase : IDisposable
+    {
+        public ResponseBase()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            GC.Collect();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
