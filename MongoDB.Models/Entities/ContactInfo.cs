0894af86caf82b5d2d54887b435785ef405f6514
mertagca <mrtagcaa@gmail.com>
2022-09-06 22:36:07 -0700

    ContactInfo Id add

--- MongoDB.Models/Entities/ContactInfo.cs
+++ MongoDB.Models/Entities/ContactInfo.cs
@@ -3,11 +3,14 @@ using BaseTypes.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace MongoDB.Models.Entities
 {
     public class ContactInfo : IContactInfo
     {
+        [JsonIgnore]
+        public string ContactInfoId { get; set; } = Guid.NewGuid().ToString();
         public InfoType InfoType { get; set; }
         public string InfoValue { get; set; }
     }
